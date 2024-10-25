12874925e0b414fe0e262a5542e03b143cb24d97
ismail noman <im.noman@asthait.com>
2024-10-25 19:33:13 +0600

    fix div class

--- blur.js
+++ blur.js
@@ -1,12 +1,9 @@
 // Function to add the blur effect
 function blurWhatsAppChats() {
     // Select all elements that display recent chat messages
-    // const recentChatTexts = document.querySelectorAll("._1hI5g ._1QtFq");
-    const recentChatTexts = document.querySelectorAll("._ak8j");
-    console.log("inside blurWhatsAppChats "+recentChatTexts);
+    const recentChatTexts = document.querySelectorAll("._ak8k");
     recentChatTexts.forEach(chat => {
       chat.classList.add("blurred-text");
-      console.log("as" +chat);
     });
   }
   console.log("WhatsApp Text Blur Extension is running!");
